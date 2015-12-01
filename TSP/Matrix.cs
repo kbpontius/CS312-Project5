068a6ecb49a3ae9ec517f8a78f6874a4a50d075a
Kyle Pontius <kbpontius@gmail.com>
2015-12-01 10:24:37 -0700

    Cleaned up Matrix w/ Resharper

--- TSP/Matrix.cs
+++ TSP/Matrix.cs
@@ -1,47 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace TSP
+﻿namespace TSP
 {
     class Matrix
     {
-        private double[,] matrix;
-        private double reductionCost = -1;
+        private readonly double[,] _matrix;
+        private double _reductionCost = -1;
 
         public Matrix(City[] cities)
         {
-            matrix = new double[cities.Length, cities.Length];
+            _matrix = new double[cities.Length, cities.Length];
 
             for(int i = 0; i < cities.Length; i++)
             {
                 for(int j = 0; j < cities.Length; j++)
                 {
-                    matrix[i, j] = cities[j].costToGetTo(cities[i]);
+                    _matrix[i, j] = cities[j].costToGetTo(cities[i]);
                 }
             }
         }
 
         public Matrix(Matrix matrix)
         {
-            this.matrix = matrix.GetMatrix();
-            this.reductionCost = matrix.GetReductionCost();
+            this._matrix = matrix.GetMatrix();
+            this._reductionCost = matrix.GetReductionCost();
         }
 
         public double[,] GetMatrix()
         {
-            return matrix;
+            return _matrix;
         }
 
         public double GetReductionCost()
         {
-            return reductionCost;
+            return _reductionCost;
         }
 
         public void SetReductionCost(double newReductionCost)
         {
-            reductionCost = newReductionCost;
+            _reductionCost = newReductionCost;
         }
     }
 }
